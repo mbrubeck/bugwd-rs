7d0afc41d2bba909f506b374d2871951eaab2360
Matt Brubeck <mbrubeck@limpet.net>
2019-02-28 13:42:07 -0800

    Caching

--- src/main.rs
+++ src/main.rs
@@ -10,26 +10,28 @@ fn get_server_port() -> u16 {
     std::env::var("PORT").ok().and_then(|p| p.parse().ok()).unwrap_or(8080)
 }
 
+macro_rules! static_file {
+    ($mime: expr, $path: expr) => {
+        Response::from_data($mime, include_str!(concat!("../resources/", $path)))
+            .with_public_cache(600)
+    }
+}
+
 fn main() {
     rouille::start_server(("0.0.0.0", get_server_port()), |request| {
         router!(request,
-            (GET) ["/"] =>
-                Response::html(include_str!("../resources/form.html")),
-            (GET) ["/search"] =>
-                Response::html(include_str!("../resources/search.html")),
-            (GET) ["/buglist.cgi"] =>
-                Response::html(include_str!("../resources/search.html")),
-            (GET) ["/static/mnemonic.js"] =>
-                Response::from_data("application/javascript", include_str!("../resources/mnemonic.js")),
-            (GET) ["/static/style.css"] =>
-                Response::from_data("text/css", include_str!("../resources/style.css")),
+            (GET) ["/"] => static_file!("text/html", "form.html"),
+            (GET) ["/search"] => static_file!("text/html", "search.html"),
+            (GET) ["/buglist.cgi"] => static_file!("text/html", "search.html"),
+            (GET) ["/static/mnemonic.js"] => static_file!("application/javascript", "mnemonic.js"),
+            (GET) ["/static/style.css"] => static_file!("text/css", "style.css"),
             (GET) ["/{mnemonic}", mnemonic: String] => {
                 let mut buf = [0; 4];
                 if let Ok(n) = mnemonic::decode(mnemonic, &mut buf[..]) {
                     buf.rotate_right(4 - n);
                     let id = u32::from_be_bytes(buf);
                     let url = format!("https://bugzilla.mozilla.org/show_bug.cgi?id={}", id);
-                    Response::redirect_303(url)
+                    Response::redirect_301(url).with_public_cache(365 * 24 * 60 * 60)
                 } else {
                     Response::empty_404()
                 }
